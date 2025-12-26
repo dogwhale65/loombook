145e590c497509045519beb96214b4d7a65d52e2
dogwhale65 <willmylius@gmail.com>
2025-12-25 20:57:04 -0600

    first commit

---
+++ src/client/java/dogwhale65/loombook/LoombookClient.java
@@ -0,0 +1,10 @@
+package dogwhale65.loombook;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LoombookClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
